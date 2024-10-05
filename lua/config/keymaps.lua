55568c975c1fb2e73cc1736f980f9a49143f1a58
Cal <accounts@cal.rip>
2024-10-05 21:23:06 +0100

    feat: set vim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move [d]own half-page and center" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move [u]p half-page and center" })
+keymap.set("n", "<C-f>", "<C-f>zz", { desc = "move DOWN [f]ull-page and center" })
+keymap.set("n", "<C-b>", "<C-b>zz", { desc = "move UP full-page and center" })
+
+keymap.set("n", "<leader>e", function()
+    local _, winid = vim.diagnostic.open_float(nil, {})
+    if not winid then
+        return
+    end
+    vim.api.nvim_win_set_config(winid or 0, { focusable = true })
+end, { noremap = true, silent = true, desc = "Diagnostics" })
