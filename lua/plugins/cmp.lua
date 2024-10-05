bf40e1ea214e9459e7ae57c283184e77f7f1c99e
Cal <accounts@cal.rip>
2024-10-05 23:54:41 +0100

    fix: cmp <cr> keymap - doesnt select

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,18 +1,21 @@
 local cmp = require("cmp")
-local auto_select = true
 
 return {
     "hrsh7th/nvim-cmp",
     opts = {
+        completion = {
+            completeopt = "menu,menuone,noinsert,noselect",
+        },
+        preselect = false,
         mapping = cmp.mapping.preset.insert({
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-a>"] = LazyVim.cmp.confirm({ select = auto_select }),
+            ["<C-a>"] = LazyVim.cmp.confirm({ select = true }),
             ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-            ["<CR>"] = function() end,
+            ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
             ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             ["<C-CR>"] = function(fallback)
                 cmp.abort()
