4c14b76044d724f4cd22b26c2dd7fd52fec415b0
Cal <accounts@cal.rip>
2024-10-05 21:23:27 +0100

    feat: set autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- --- Don't create a comment string when hitting <Enter> on a comment line
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("DisableNewLineAutoCommentString", {}),
+    callback = function()
+        vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+    end,
+})
