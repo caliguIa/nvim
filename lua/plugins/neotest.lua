4f51b9e5d88f818754132d4b3640890a06ef7479
Cal <accounts@cal.rip>
2024-10-06 13:51:30 +0100

    feat: setup neotest for phpunit & vitest

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "antoinemadec/FixCursorHold.nvim",
+        "olimorris/neotest-phpunit",
+        "marilari88/neotest-vitest",
+    },
+    opts = {
+        adapters = {
+            ["neotest-phpunit"] = {},
+            ["neotest-vitest"] = {
+                -- vitestCommand = "bunx vitest",
+                cwd = function(file)
+                    local util = require("neotest-vitest.util")
+                    return util.find_node_modules_ancestor(file)
+                end,
+            },
+        },
+    },
+}
