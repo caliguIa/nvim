b62f3f9700091f19b2159abbf5b1b8db40fab8f0
Cal <accounts@cal.rip>
2024-10-06 23:46:07 +0100

    feat: refactor keymaps & disable macros/marks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,26 +1,30 @@
-local keymap = vim.keymap
+local set = vim.keymap.set
+local delete = vim.keymap.del
 
--- Remove default keymaps
-keymap.del('n', 'g%')
-keymap.del('n', '<leader>K')
-keymap.del('n', '<leader>L')
-keymap.del('n', '<leader>`')
-keymap.del('n', '<leader>ft')
+delete('n', 'g%')
+delete('n', '<leader>K')
+delete('n', '<leader>L')
+delete('n', '<leader>`')
+delete('n', '<leader>ft')
 
-keymap.set('n', '<leader>X', '<cmd>!chmod +x %<CR>', { desc = 'Make file executable' })
+set('n', 'q', '<Nop>', { desc = 'Disable macro recording because I do not use it' })
+set('n', 'm', '<Nop>', { desc = 'Disable marks ecause I do not use them' })
+set('n', 'x', '"_x', { desc = 'Delete character without copying' })
 
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+set('n', '<leader>X', '<cmd>!chmod +x %<CR>', { desc = 'Make file executable' })
 
-keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'move [d]own half-page and center' })
-keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'move [u]p half-page and center' })
-keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'move DOWN [f]ull-page and center' })
-keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
+set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
 
-keymap.set('n', '<leader>e', function()
+set('n', '<C-d>', '<C-d>zz', { desc = 'move [d]own half-page and center' })
+set('n', '<C-u>', '<C-u>zz', { desc = 'move [u]p half-page and center' })
+set('n', '<C-f>', '<C-f>zz', { desc = 'move DOWN [f]ull-page and center' })
+set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
+
+set('n', '<leader>e', function()
     local _, winid = vim.diagnostic.open_float(nil, {})
     if not winid then return end
-    vim.api.nvim_win_set_config(winid or 0, { focusable = true })
+    vim.api.nvim_win_config(winid or 0, { focusable = true })
 end, { noremap = true, silent = true, desc = 'Diagnostics' })
 
-keymap.set('n', '<leader>ft', function() LazyVim.terminal() end, { desc = 'Terminal' })
+set('n', '<leader>ft', function() LazyVim.terminal() end, { desc = 'Terminal' })
