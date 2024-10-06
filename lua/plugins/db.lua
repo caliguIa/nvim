f053bfd2de6f0fc0f1ca2380f630d65f4d9781c4
Cal <accounts@cal.rip>
2024-10-06 13:51:11 +0100

    feat: setup dadbod plugin

---
+++ lua/plugins/db.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod", lazy = true },
+            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            local cmp = require("cmp")
+            cmp.setup.filetype({ "sql" }, { sources = { { name = "vim-dadbod-completion" }, { name = "buffer" } } })
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+        keys = {
+            {
+                "<leader>db",
+                "<CMD>DBUITOGGLE<CR>",
+                desc = "Toggle database",
+            },
+        },
+    },
+}
