877de2fd7ddc133fe6c5c6a1e345a444772aa9d1
Cal <accounts@cal.rip>
2024-10-08 22:30:08 +0100

    feat: trying out blink.cmp instead of nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,30 +1,50 @@
-local cmp = require 'cmp'
+-- local cmp = require 'cmp'
 
 return {
-    'hrsh7th/nvim-cmp',
-    opts = {
-        completion = {
-            completeopt = 'menu,menuone,noinsert,noselect',
-        },
-        preselect = false,
-        mapping = cmp.mapping.preset.insert {
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-            ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-a>'] = LazyVim.cmp.confirm { select = true },
-            ['<C-y>'] = LazyVim.cmp.confirm { select = true },
-            ['<CR>'] = LazyVim.cmp.confirm { select = false },
-            ['<S-CR>'] = LazyVim.cmp.confirm { behavior = cmp.ConfirmBehavior.Replace }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            ['<C-CR>'] = function(fallback)
-                cmp.abort()
-                fallback()
-            end,
-        },
-        experimental = {
-            native_menu = false,
-            ghost_text = false,
+    {
+        'hrsh7th/nvim-cmp',
+        enabled = false,
+        -- opts = {
+        --     completion = {
+        --         completeopt = 'menu,menuone,noinsert,noselect',
+        --     },
+        --     preselect = false,
+        --     mapping = cmp.mapping.preset.insert {
+        --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        --         ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        --         ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        --         ['<C-Space>'] = cmp.mapping.complete(),
+        --         ['<C-a>'] = LazyVim.cmp.confirm { select = true },
+        --         ['<C-y>'] = LazyVim.cmp.confirm { select = true },
+        --         ['<CR>'] = LazyVim.cmp.confirm { select = false },
+        --         ['<S-CR>'] = LazyVim.cmp.confirm { behavior = cmp.ConfirmBehavior.Replace }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        --         ['<C-CR>'] = function(fallback)
+        --             cmp.abort()
+        --             fallback()
+        --         end,
+        --     },
+        --     experimental = {
+        --         native_menu = false,
+        --         ghost_text = false,
+        --     },
+        -- },
+    },
+    {
+        'saghen/blink.cmp',
+        lazy = false, -- lazy loading handled internally
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = 'v0.*',
+        opts = {
+            highlight = { use_nvim_cmp_as_default = true },
+            nerd_font_variant = 'normal',
+            accept = { auto_brackets = { enabled = true } },
+            trigger = { signature_help = { enabled = false, show_on_insert_on_trigger_character = false } },
+            keymap = {
+                accept = '<C-a>',
+                select_prev = { '<Up>', '<C-k>', '<C-p>' },
+                select_next = { '<Down>', '<C-j>', '<C-n>' },
+            },
         },
     },
 }
--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -17,8 +17,8 @@ return {
         },
         init = function()
             local base = vim.fs.joinpath(os.getenv 'HOME', 'tmp', 'queries')
-            local cmp = require 'cmp'
-            cmp.setup.filetype({ 'sql' }, { sources = { { name = 'vim-dadbod-completion' }, { name = 'buffer' } } })
+            -- local cmp = require 'cmp'
+            -- cmp.setup.filetype({ 'sql' }, { sources = { { name = 'vim-dadbod-completion' }, { name = 'buffer' } } })
 
             vim.g.db_ui_use_nerd_fonts = 1
             vim.g.db_ui_winwidth = 40
