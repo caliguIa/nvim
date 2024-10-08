5e0e5890f0a5ed7929a5d928a846a776ae00cd4b
Cal <accounts@cal.rip>
2024-10-08 22:26:52 +0100

    feat: disable unused plugins

--- lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-    { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
-    -- { "rcarriga/nvim-notify", enabled = false },
-    -- { "folke/noice.nvim", enabled = false },
-}
---
+++ lua/plugins/zdisabled.lua
@@ -0,0 +1,9 @@
+return {
+    { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
+    { 'rcarriga/nvim-notify', enabled = false },
+    { 'folke/noice.nvim', enabled = false },
+    { 'hrsh7th/nvim-cmp', enabled = false },
+    { 'folke/tokyonight.nvim', enabled = false },
+    { 'L3MON4D3/LuaSnip', enabled = false },
+    { 'akinsho/bufferline.nvim', enabled = false },
+}
