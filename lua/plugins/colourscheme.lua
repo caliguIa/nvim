52c90f54bcae049b16a11bd7e1538bf759e235ca
Cal <accounts@cal.rip>
2024-10-08 09:58:49 +0100

    chore: cleanup, update deps

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,20 +1,5 @@
 return {
     {
-        'olivercederborg/poimandres.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            local p = require 'poimandres.palette'
-            require('poimandres').setup {
-                highlight_groups = {
-                    LspReferenceText = { bg = p.background1 },
-                    LspReferenceRead = { bg = p.background1 },
-                    LspReferenceWrite = { bg = p.background1 },
-                },
-            }
-        end,
-    },
-    {
         'LazyVim/LazyVim',
         opts = {
             colorscheme = 'catppuccin-mocha',
