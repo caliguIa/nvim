b16cc503f572f3ceb10269a8d215aff93d878f1d
Cal <accounts@cal.rip>
2024-10-08 22:29:13 +0100

    feat: add autocmd to disable corn diags in insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
     group = vim.api.nvim_create_augroup('DisableNewLineAutoCommentString', {}),
     callback = function() vim.opt.formatoptions = vim.opt.formatoptions - { 'c', 'r', 'o' } end,
 })
+-- Disable corn errors when in insert mode
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'InsertLeave' }, {
+    group = vim.api.nvim_create_augroup('DisableCornInsert', {}),
+    callback = function() require('corn').toggle() end,
+})
