d1575684dad06d3c6d2aba4c45ad66cfc5f0305c
Cal <acc@calrichards.io>
2024-10-09 14:51:12 +0100

    feat: setup css lsp

---
+++ lua/plugins/css.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "css-lsp",
+                "cssmodules-language-server",
+                "css-variables-language-server",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                cssmodules_ls = {
+                    enabled = true,
+                },
+                css_variables = {
+                    enabled = true,
+                },
+                css_ls = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+}
