3a3ff63e0ca948035918571117205987c7bce002
Cal <acc@calrichards.io>
2024-10-10 22:46:21 +0100

    feat: disable corn, trialing tiny-inline-diagnostics

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,29 @@
+local icons = LazyVim.config.icons.diagnostics
+
+return {
+    -- "RaafatTurki/corn.nvim",
+    -- cmd = "Corn",
+    -- event = "LspAttach",
+    -- opts = {
+    --     icons = {
+    --         error = icons.Error,
+    --         warn = icons.Warn,
+    --         hint = icons.Hint,
+    --         info = icons.Info,
+    --     },
+    --     item_preprocess_func = function(item) return item end,
+    -- },
+    -- keys = {
+    --     {
+    --         "n",
+    --         "<leader>E",
+    --         '<cmd>lua require("corn").toggle()<CR>',
+    --         { noremap = true, silent = true, desc = "[E]rrors [t]oggle" },
+    --     },
+    -- },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "LspAttach",
+        config = function() require("tiny-inline-diagnostic").setup() end,
+    },
+}
