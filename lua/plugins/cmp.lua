40e4feeb70a28d597bae4bd32a59c6b7ada15026
Cal <acc@calrichards.io>
2024-11-04 10:39:14 +0000

    fix(blink.cmp): update keymap to support latest version

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,9 +3,9 @@ return {
         "saghen/blink.cmp",
         lazy = false,
         dependencies = {
-            -- "saghen/blink.compat",
+            -- { "saghen/blink.compat", opts = { impersonate_nvim_cmp = true } },
             "rafamadriz/friendly-snippets",
-            -- "kristijanhusak/vim-dadbod-completion",
+            -- { "kristijanhusak/vim-dadbod-completion", lazy = true },
         },
         version = "v0.*",
         ---@module 'blink.cmp'
@@ -13,43 +13,22 @@ return {
         opts = {
             highlight = { use_nvim_cmp_as_default = true },
             -- sources = {
-            -- completion = {
-            --     enabled_providers = { "lsp", "path", "snippets", "buffer" },
-            -- },
-
-            -- providers = {
-            --     lazydev = {
-            --         name = "lazydev",
-            --         module = "blink.compat",
-            --         score_offset = 3,
-            --         opts = {},
+            --     completion = {
+            --         enabled_providers = { "lsp", "path", "snippets", "buffer", "vim-dadbod-completion" },
             --     },
-            -- ["vim-dadbod-completion"] = {
-            --     name = "vim-dadbod-completion",
-            --     module = "blink.compat",
-            --     score_offset = 3,
-            --     opts = {},
-            -- },
-            -- },
+            --
+            --     providers = {
+            --         ["vim-dadbod-completion"] = {
+            --             name = "vim-dadbod-completion",
+            --             module = "blink.compat.source",
+            --             score_offset = 3,
+            --             -- opts = {},
+            --         },
+            --     },
             -- },
             keymap = {
-                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-                ["<C-e>"] = { "hide" },
-                ["<C-y>"] = { "select_and_accept" },
+                preset = "default",
                 ["<C-a>"] = { "select_and_accept" },
-
-                ["<C-p>"] = { "select_prev", "fallback" },
-                ["<C-n>"] = { "select_next", "fallback" },
-                ["<C-k>"] = { "select_prev", "fallback" },
-                ["<C-j>"] = { "select_next", "fallback" },
-                ["<Up>"] = { "select_prev", "fallback" },
-                ["<Down>"] = { "select_next", "fallback" },
-
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-                ["<Tab>"] = { "snippet_forward", "fallback" },
-                ["<S-Tab>"] = { "snippet_backward", "fallback" },
             },
         },
     },
