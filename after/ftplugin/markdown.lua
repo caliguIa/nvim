659390f953bf9576ec52dbdaf2b9bccfe58b2fef
Cal <acc@calrichards.io>
2024-11-06 10:56:52 +0000

    fix(markdown): disable spell and diagnostics if todo file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,16 @@
 vim.cmd "setlocal spell wrap"
+
+if vim.b.did_ftplugin then return end
+vim.b.did_ftplugin = 1
+
+-- Function to check if current file is in a todo directory
+local function is_in_todo_dir()
+    local filepath = vim.fn.expand "%:p"
+    return string.find(filepath:lower(), "/todo/") ~= nil
+end
+
+-- Disable spell checking and diagnostics for todo files
+if is_in_todo_dir() then
+    vim.opt_local.spell = false
+    vim.diagnostic.enable(false)
+end
