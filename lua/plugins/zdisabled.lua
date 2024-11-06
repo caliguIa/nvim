b9d103fd7b5ae65d06e1cf63a6e6d754a6af74d0
Cal <acc@calrichards.io>
2024-11-06 10:44:23 +0000

    chore(markdown): disable preview plugin

--- lua/plugins/zdisabled.lua
+++ lua/plugins/zdisabled.lua
@@ -5,4 +5,5 @@ return {
     { "hrsh7th/nvim-cmp", enabled = false },
     { "folke/tokyonight.nvim", enabled = false },
     { "L3MON4D3/LuaSnip", enabled = false },
+    { "iamcco/markdown-preview.nvim", enabled = false },
 }
