bf3c851be57db6bbd46c739a3b985c0efb4915f0
Cal <acc@calrichards.io>
2024-11-06 10:47:53 +0000

    fix(cmp): keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,34 +1,83 @@
 return {
     {
         "saghen/blink.cmp",
-        lazy = false,
-        dependencies = {
-            -- { "saghen/blink.compat", opts = { impersonate_nvim_cmp = true } },
-            "rafamadriz/friendly-snippets",
-            -- { "kristijanhusak/vim-dadbod-completion", lazy = true },
+        optional = false,
+        enabled = true,
+        -- dependencies = {
+        --     { "saghen/blink.compat", opts = {} },
+        --     {
+        --         "zbirenbaum/copilot-cmp",
+        --         dependencies = "copilot.lua",
+        --         opts = {},
+        --         config = function(_, opts)
+        --             local copilot_cmp = require "copilot_cmp"
+        --             copilot_cmp.setup(opts)
+        --             LazyVim.lsp.on_attach(function(client) copilot_cmp._on_insert_enter {} end, "copilot")
+        --         end,
+        --     },
+        -- },
+        specs = {
+            {
+                "zbirenbaum/copilot.lua",
+                event = "InsertEnter",
+                opts = {
+                    suggestion = {
+                        enabled = true,
+                        auto_trigger = true,
+                        keymap = { accept = false },
+                    },
+                },
+            },
         },
-        version = "v0.*",
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
-            highlight = { use_nvim_cmp_as_default = true },
+            windows = {
+                ghost_text = {
+                    enabled = false,
+                },
+            },
             -- sources = {
             --     completion = {
-            --         enabled_providers = { "lsp", "path", "snippets", "buffer", "vim-dadbod-completion" },
-            --     },
-            --
-            --     providers = {
-            --         ["vim-dadbod-completion"] = {
-            --             name = "vim-dadbod-completion",
-            --             module = "blink.compat.source",
-            --             score_offset = 3,
-            --             -- opts = {},
-            --         },
+            --         -- remember to enable your providers here
+            --         enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
             --     },
             -- },
+            -- providers = {
+            --     digraphs = {
+            --         name = "copilot", -- IMPORTANT: use the same name as you would for nvim-cmp
+            --         module = "blink.compat.source",
+            --
+            --         -- all blink.cmp source config options work as normal:
+            --         score_offset = -3,
+            --
+            --         opts = {},
+            --     },
+            -- },
+            highlight = { use_nvim_cmp_as_default = true },
             keymap = {
                 preset = "default",
-                ["<C-a>"] = { "select_and_accept" },
+                ["<Tab>"] = {
+                    function(cmp)
+                        if cmp.is_in_snippet() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_and_accept()
+                        end
+                    end,
+                    "snippet_forward",
+                    "fallback",
+                },
+                ["<C-a>"] = {
+                    function()
+                        if require("copilot.suggestion").is_visible() then
+                            LazyVim.create_undo()
+                            require("copilot.suggestion").accept()
+                            return true
+                        else
+                        end
+                    end,
+                },
             },
         },
     },
