e2100019c336c4b3c2b62846c76dd96a69a7a6cd
Cal <acc@calrichards.io>
2024-12-27 10:29:46 +0000

    fix: pin lazy lock

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,75 +1,35 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {
-            default_file_explorer = true,
-            columns = { "icon" },
-            win_options = {
-                wrap = false,
-                signcolumn = "no",
-                cursorcolumn = false,
-                foldcolumn = "0",
-                spell = false,
-                list = false,
-                conceallevel = 3,
-                concealcursor = "nvic",
-            },
-            lsp_file_methods = {
-                -- Enable or disable LSP file operations
-                enabled = true,
-                -- Time to wait for LSP file operations to complete before skipping
-                timeout_ms = 1000,
-                -- Set to true to autosave buffers that are updated with LSP willRenameFiles
-                -- Set to "unmodified" to only save unmodified buffers
-                autosave_changes = true,
-            },
-            watch_for_changes = true,
-            view_options = {
-                show_hidden = true,
-            },
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-s>"] = {
-                    "actions.select",
-                    opts = { vertical = true },
-                    desc = "Open the entry in a vertical split",
-                },
-                ["<C-h>"] = {
-                    "actions.select",
-                    opts = { horizontal = true },
-                    desc = "Open the entry in a horizontal split",
-                },
-                ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-                ["<C-p>"] = "actions.preview",
-                ["<C-c>"] = "actions.close",
-                ["q"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["`"] = "actions.cd",
-                ["~"] = {
-                    "actions.cd",
-                    opts = { scope = "tab" },
-                    desc = ":tcd to the current oil directory",
-                    mode = "n",
-                },
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-                ["g\\"] = "actions.toggle_trash",
-            },
+    "stevearc/oil.nvim",
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+    opts = {
+        lsp_file_methods = {
+            autosave_changes = true,
         },
-        keys = {
-            {
-                "<leader>fe",
-                "<CMD>Oil<CR>",
-                desc = "File explorer",
-            },
+        view_options = {
+            show_hidden = true,
         },
-        dependencies = { { "echasnovski/mini.icons", opts = {} } },
-        config = function(_, opts) require("oil").setup(opts) end,
+        keymaps = {
+            ["q"] = "actions.close",
+        },
+    },
+    config = function(_, opts)
+        require("oil").setup(opts)
+
+        -- Auto-open Oil when starting Neovim with no file arguments
+        vim.api.nvim_create_autocmd("UIEnter", {
+            callback = function()
+                vim.schedule(function()
+                    if vim.fn.argc() == 0 then vim.cmd "Oil" end
+                end)
+            end,
+        })
+    end,
+    keys = {
+        {
+            "<leader>fe",
+            "<CMD>Oil<CR>",
+            desc = "File explorer",
+        },
     },
 }
