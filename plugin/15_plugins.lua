3fad5c949c6101b89f12bbf7a71a3ddfd928a39c
Cal <acc@calrichards.io>
2025-02-25 12:26:16 +0000

    feat: add path to oil

--- plugin/15_plugins.lua
+++ plugin/15_plugins.lua
@@ -27,10 +27,24 @@ later(function()
 end)
 later(function()
     add("stevearc/oil.nvim")
+    function _G.get_oil_winbar()
+        local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+        local dir = require("oil").get_current_dir(bufnr)
+        if dir then
+            return vim.fn.fnamemodify(dir, ":~")
+        else
+            -- If there is no current directory (e.g. over ssh), just show the buffer name
+            return vim.api.nvim_buf_get_name(0)
+        end
+    end
     require("oil").setup({
         lsp_file_methods = { autosave_changes = true },
         view_options = { show_hidden = true },
         keymaps = { ["q"] = "actions.close" },
+        watch_for_changes = false,
+        win_options = {
+            winbar = "%!v:lua.get_oil_winbar()",
+        },
     })
 end)
 later(function()
